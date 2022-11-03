8a0f5c628e3e98cae23128560ad923bf3c3eaa82
htaylor91 <htaylor91@gmail.com>
2022-11-03 17:08:29 +0100

    feat(index.js): 🔨 update imports to index.js to include all helpers and factories

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,20 @@
 import { recipeFactory, recipes } from "./factories/recipeFactory.js";
+import { } from "./factories/dropdownFactory.js";
+import { } from "./helpers/keyboard.js";
+import { } from "./helpers/inputs.js";
+import { } from "./helpers/tags.js";
+import { } from "./helpers/tagObserver.js"
+import { } from "./helpers/recipeObserver.js"
 
+const recipesOutput = document.getElementById("recipesOutput");
 //Function to display all recipe cards
 const displayData = recipes => {
-    const recipesSection = document.querySelector("[data-recipes-section]");
+    
     recipes.forEach((recipe) => {
         //for each recipe in the array recipes[] ...
-        const recipeModel = recipeFactory(recipe);
-        const recipeCardDOM = recipeModel.recipeCardDOM();
-        recipesSection.appendChild(recipeCardDOM);
+        const recipeModel = recipeFactory(recipe); //The value of recipeModel is the return value of recipeFactory(recipe)
+        const recipeCardDOM = recipeModel.recipeCardDOM(); //recipeCardDOM() returns the recipe's DOM
+        recipesOutput.appendChild(recipeCardDOM); //Append each recipe to the recipesOutput section
     });
 }
 
