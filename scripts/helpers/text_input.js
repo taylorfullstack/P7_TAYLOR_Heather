311eb3c1ba310995d8d6494b90e20cc62735e494
htaylor91 <htaylor91@gmail.com>
2022-11-29 17:04:44 +0100

    feat(text_input.js): ✨ add lettersSpacesApostrophesOnly function to validate user input against regex

--- scripts/helpers/text_input.js
+++ scripts/helpers/text_input.js
@@ -1,5 +1,14 @@
 
 //Function to convert strings to the NFD normalized Unicode format
 //Then, replace accents with empty strings
-export const removeAccents = (string) =>
-string.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+export const removeAccents = (string) => string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+//Test user input against Regex
+export const lettersSpacesApostrophesOnly = (userTextInput) => {
+	let lettersSpacesApostrophes = /^[A-Za-z\s']+$/g;
+	if(userTextInput.match(lettersSpacesApostrophes)){
+		return true;
+	} else { 
+		return false;
+	}
+}
