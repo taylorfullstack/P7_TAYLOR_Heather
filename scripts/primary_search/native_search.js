391166a07df784899f5f2c3a039957a9f9174486
htaylor91 <htaylor91@gmail.com>
2022-11-30 11:31:36 +0100

    feat(native_search.js): ✨ add searchRecipes() event to search recipes by name, ingredients, or description on user input

---
+++ scripts/primary_search/native_search.js
@@ -0,0 +1,110 @@
+
+import { lettersSpacesApostrophesOnly, removeAccents } from "../helpers/text_input.js";
+
+const searchInput = document.getElementById("searchInput");
+const searchError = document.getElementById("searchError");
+
+let userText;
+let userTextInput;
+let words = [];
+let deletion;
+
+const recipesCollection = document.getElementsByTagName("article");
+
+export function searchRecipes(event) {
+	userText = event.target.value;
+	userTextInput = removeAccents(userText.toLowerCase().trim()); 
+
+	if(lettersSpacesApostrophesOnly(userTextInput) === false && (userTextInput !== "")){
+		searchError.dataset.searchErrorVisible = "true";
+		return;
+	}
+	
+	searchError.dataset.searchErrorVisible = "false";
+
+	words = userTextInput.split(" ");
+    
+	if(deletion === true){
+		displayRestoredRecipes();
+		return;
+	}
+
+	if (deletion === false && (userTextInput.length >= 3)) {    
+		hideSearchEliminatedRecipes();
+		return;
+	}
+}
+
+async function updateAllRecipes(collection){
+	try {
+		for (let i = 0; i < collection.length; i++) {
+			searchRecipeForAllWords(collection[i]);
+			eliminateOrRestoreRecipe(collection[i]);
+		}
+	} catch (error) {
+		return false;
+	}
+
+	return true;
+}
+
+async function searchRecipeForAllWords(recipe) {
+	const recipeTitle = removeAccents(recipe.dataset.recipeArticle); 
+	const recipeIngredients = removeAccents(recipe.dataset.recipeIngredients);
+	const recipeDescription = (removeAccents(recipe.querySelector("[data-recipe-description]").textContent.toLowerCase())).replaceAll(/[,.:()]+/g, "");
+
+	let results = "";
+	for (let i = 0; i < words.length; i++) {
+		let result = searchRecipeForWord(words[i], recipeTitle, recipeIngredients, recipeDescription);
+		results += `${result} `;
+	}
+
+	return results;
+}
+
+function searchRecipeForWord(word, recipeTitle, recipeIngredients, recipeDescription) {
+	if(words.at(0) === "") return undefined;
+	if(recipeTitle.includes(word) || recipeIngredients.includes(word) || recipeDescription.includes(word)) return true;
+
+	return false;
+}
+
+async function eliminateOrRestoreRecipe(recipe) {
+	const results = await searchRecipeForAllWords(recipe);
+	if(results.includes("undefined") || !results.includes("false")){
+		recipe.classList.remove("searchEliminated");
+	}
+
+	if(results.includes("false") && !recipe.classList.contains("searchEliminated")){
+		recipe.classList.add("searchEliminated");
+	}
+}
+
+const hideSearchEliminatedRecipes = async () => {
+	const updateComplete = await updateAllRecipes(recipesCollection);
+	if(updateComplete){
+		const eliminatedList = document.querySelectorAll(".searchEliminated");
+		for(let i = 0; i < eliminatedList.length; i++){
+			eliminatedList[i].classList.replace("found", "notfound");
+		}
+	}
+}
+
+const displayRestoredRecipes = async () => {
+	const updateComplete = await updateAllRecipes(recipesCollection);
+	if(updateComplete){
+		const hiddenList = document.querySelectorAll(".notfound");
+		for(let i = 0; i < hiddenList.length; i++){
+			if(!hiddenList[i].classList.contains("tagEliminated") && !hiddenList[i].classList.contains("searchEliminated")){
+				hiddenList[i].classList.replace("notfound", "found");
+			};
+		}
+	}
+}
+
+function detectDeletion(event){
+	(event.key === "Backspace" || event.key === "Delete") ? (deletion = true) : (deletion = false);
+}
+
+searchInput.addEventListener("input", searchRecipes);
+searchInput.addEventListener("keydown", detectDeletion);
