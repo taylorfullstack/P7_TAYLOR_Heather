19c89d6d98cf044e793643555edfeb984ed8a831
htaylor91 <htaylor91@gmail.com>
2022-12-02 11:52:10 +0100

    feat(functional_search.js): ✨ update detectDeletion() to listen for CTRL + X, reorder functions to improve code readability

--- scripts/primary_search/functional_search.js
+++ scripts/primary_search/functional_search.js
@@ -1,15 +1,20 @@
-
 import { lettersSpacesApostrophesOnly, removeAccents } from "../helpers/text_input.js";
 
 const searchInput = document.getElementById("searchInput");
 const searchError = document.getElementById("searchError");
+const recipesCollection = document.getElementsByTagName("article");
 
 let userText;
 let userTextInput;
 let words = [];
 let deletion;
 
-const recipesCollection = document.getElementsByTagName("article");
+function detectDeletion(event){
+	let xKey = event.key === "x" || event.key ==="X";
+	let metaKey = event.metaKey;
+	let ctrlKey = event.ctrlKey;
+	(event.key === "Backspace" || event.key === "Delete" || (ctrlKey && xKey) || (metaKey && xKey)) ? (deletion = true) : (deletion = false);
+}
 
 export function searchRecipes(event) {
 	userText = event.target.value;
@@ -26,20 +31,41 @@ export function searchRecipes(event) {
     
 	if(deletion === true){
 		displayRestoredRecipes();
-		hideSearchEliminatedRecipes();
 		return;
 	}
 
-	if (deletion === false && (userTextInput.length >= 3)) {    
+	if (deletion === false && (userTextInput.length >= 3)){
 		hideSearchEliminatedRecipes();
+		displayRestoredRecipes();
 		return;
+	}
+}
+
+async function hideSearchEliminatedRecipes(){
+	const updateComplete = await updateAllRecipes(recipesCollection);
+	if(updateComplete){
+		const eliminatedList = document.querySelectorAll(".searchEliminated");
+		eliminatedList.forEach((recipe => {
+			recipe.classList.replace("found", "notfound");
+		}));
+	}
+}
+
+async function displayRestoredRecipes(){
+	const updateComplete = await updateAllRecipes(recipesCollection);
+	if(updateComplete){
+		const hiddenList = document.querySelectorAll(".notfound");
+		hiddenList.forEach((recipe => {
+			if(!recipe.classList.contains("tagEliminated") && !recipe.classList.contains("searchEliminated")){
+				recipe.classList.replace("notfound", "found");
+			};
+		}));
 	}
 }
 
 async function updateAllRecipes(collection){
 	try {
 		Array.from(collection).forEach(recipe => {
-			searchRecipeForAllWords(recipe);
 			eliminateOrRestoreRecipe(recipe);
 		});
 		
@@ -48,6 +74,17 @@ async function updateAllRecipes(collection){
 	}
 
 	return true;
+}
+
+async function eliminateOrRestoreRecipe(recipe) {
+	const results = await searchRecipeForAllWords(recipe);
+	if(results.includes(undefined) || !results.includes(false)){
+		recipe.classList.remove("searchEliminated");
+	}
+
+	if(results.includes(false) && !recipe.classList.contains("searchEliminated")){
+		recipe.classList.add("searchEliminated");
+	}
 }
 
 async function searchRecipeForAllWords(recipe) {
@@ -70,42 +107,5 @@ function searchRecipeForWord(word, recipeTitle, recipeIngredients, recipeDescrip
 	return false;
 }
 
-async function eliminateOrRestoreRecipe(recipe) {
-	const results = await searchRecipeForAllWords(recipe);
-	if(results.includes(undefined) || !results.includes(false)){
-		recipe.classList.remove("searchEliminated");
-	}
-
-	if(results.includes(false) && !recipe.classList.contains("searchEliminated")){
-		recipe.classList.add("searchEliminated");
-	}
-}
-
-const hideSearchEliminatedRecipes = async () => {
-	const updateComplete = await updateAllRecipes(recipesCollection);
-	if(updateComplete){
-		const eliminatedList = document.querySelectorAll(".searchEliminated");
-		eliminatedList.forEach((recipe => {
-			recipe.classList.replace("found", "notfound");
-		}));
-	}
-}
-
-const displayRestoredRecipes = async () => {
-	const updateComplete = await updateAllRecipes(recipesCollection);
-	if(updateComplete){
-		const hiddenList = document.querySelectorAll(".notfound");
-		hiddenList.forEach((recipe => {
-			if(!recipe.classList.contains("tagEliminated") && !recipe.classList.contains("searchEliminated")){
-				recipe.classList.replace("notfound", "found");
-			};
-		}));
-	}
-}
-
-function detectDeletion(event){
-	(event.key === "Backspace" || event.key === "Delete") ? (deletion = true) : (deletion = false);
-}
-
-searchInput.addEventListener("input", searchRecipes);
 searchInput.addEventListener("keydown", detectDeletion);
+searchInput.addEventListener("input", searchRecipes);
