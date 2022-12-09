df4f5fd54a9ab4274eccf9450a5b5644988808a8
htaylor91 <htaylor91@gmail.com>
2022-12-09 11:45:36 +0100

    feat(app.js): 🚚 update path in app.js to import native_search.js

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ import { } from "./components/dropdown/dropdown_aria.js";
 import { } from "./components/tag_menu/tags.js";
 import { } from "./components/tag_menu/tag_observer.js";
 import { } from "./components/recipes_output/recipe_observer.js";
-import { } from "./primary_search/functional_search.js";
+import { } from "./primary_search/native_search.js";
 
 const appliancesList =  Array.from(menuAppliances.children);
 const utensilsList =  Array.from(menuUtensils.children);
